7ad82fa37375ebb28d8adfe9c945993cfe3b2e37
tp323 <tiagopardl@hotmail.com>
2021-12-19 00:57:16 +0000

    queries 1 DONE

---
+++ queries.sql
@@ -0,0 +1,17 @@
+--a)
+select nome, modelo, marca, localizacao from ACTIVO where estado = '1';
+
+--b)
+--select nome, email, profissao from PESSOA inner join COMPETENCIA on ;
+
+--c)
+
+
+--d)
+
+
+--e)
+
+
+--f)
+
