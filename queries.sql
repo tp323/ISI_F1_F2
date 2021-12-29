b2c1216de62a84f9513e59b8c9a70c7150d3b77d
tp323 <tiagopardl@hotmail.com>
2021-12-29 02:30:54 +0000

    2.d  2.e  4 DONE

--- queries.sql
+++ queries.sql
@@ -15,29 +15,42 @@ select empresa.nome, nipc, url, PESSOA.nome from (EMPRESA full outer join PESSOA
 select distinct ACTIVO.nome, PESSOA.equipa, PESSOA.nome from 
 (PESSOA full outer join INTER_EQUIPA on PESSOA.equipa=INTER_EQUIPA.equipa full outer join INTERVENCAO on INTER_EQUIPA.intervencao=INTERVENCAO.num left outer join ACTIVO on INTERVENCAO.activo=ACTIVO.id)
 where 
---(INTERVENCAO.estado = 'em execução' and ACTIVO.nome = 'válvula de ar condicionado') or 
-ACTIVO.nome = 'válvula de ar condicionado';
+((INTERVENCAO.estado = 'em execução' or INTERVENCAO.estado = 'em análise') 
+and ACTIVO.nome = 'válvula de ar condicionado')
+;
+--gere
+select distinct ACTIVO.nome, PESSOA.equipa, PESSOA.nome from (PESSOA full outer join ACTIVO on PESSOA.id=ACTIVO.pessoa) where ACTIVO.nome = 'válvula de ar condicionado'; 
+
+
 select distinct ACTIVO.nome, PESSOA.equipa, PESSOA.nome, ACTIVO.pessoa from 
 (PESSOA full outer join INTER_EQUIPA on PESSOA.equipa=INTER_EQUIPA.equipa full outer join INTERVENCAO on INTER_EQUIPA.intervencao=INTERVENCAO.num full outer join ACTIVO 
 on INTERVENCAO.activo=ACTIVO.id)-- join ACTIVO on PESSOA.id = ACTIVO.pessoa)
 where ACTIVO.nome = 'válvula de ar condicionado';
 
+--select ACTIVO.nome from (INTERVENCAO right outer join ACTIVO on INTERVENCAO.activo = ACTIVO.id) ;
+
+select distinct ACTIVO.nome, PESSOA.equipa, PESSOA.nome from 
+(PESSOA full outer join INTER_EQUIPA on PESSOA.equipa=INTER_EQUIPA.equipa full outer join INTERVENCAO on INTER_EQUIPA.intervencao=INTERVENCAO.num left outer join ACTIVO on INTERVENCAO.activo=ACTIVO.id)
+where 
+((INTERVENCAO.estado = 'em execução' or INTERVENCAO.estado = 'em análise') 
+and ACTIVO.nome = 'válvula de ar condicionado')
+union 
 select distinct ACTIVO.nome, PESSOA.equipa, PESSOA.nome from (PESSOA full outer join ACTIVO on PESSOA.id=ACTIVO.pessoa) where ACTIVO.nome = 'válvula de ar condicionado'; 
 
-
-select ACTIVO.nome from (INTERVENCAO right outer join ACTIVO on INTERVENCAO.activo = ACTIVO.id) ;
-
-select nome from ACTIVO;
 
 --e)
 --falta juntar as 2 queries para um resultado conjunto
 --geridos por Manuel Fernandes
-select ACTIVO.nome as nomeGeridos from (ACTIVO inner join PESSOA as p on ACTIVO.pessoa = p.id) where p.nome = 'Manuel Fernandes';
-
 select ACTIVO.nome from (ACTIVO inner join PESSOA on ACTIVO.pessoa = PESSOA.id) where PESSOA.nome = 'Manuel Fernandes';
 
 --intervencionados por Manuel Fernandes
 select ACTIVO.nome as nomeInterv from (ACTIVO inner join INTERVENCAO on ACTIVO.id = INTERVENCAO.activo join INTER_EQUIPA on INTERVENCAO.num = INTER_EQUIPA.intervencao join PESSOA on INTER_EQUIPA.equipa = PESSOA.equipa) where PESSOA.nome ='Manuel Fernandes';
+
+select ACTIVO.nome from (ACTIVO inner join PESSOA on ACTIVO.pessoa = PESSOA.id) where PESSOA.nome = 'Manuel Fernandes'
+union 
+select ACTIVO.nome as nomeInterv from (ACTIVO inner join INTERVENCAO on ACTIVO.id = INTERVENCAO.activo join INTER_EQUIPA on INTERVENCAO.num = INTER_EQUIPA.intervencao join PESSOA on INTER_EQUIPA.equipa = PESSOA.equipa) where PESSOA.nome ='Manuel Fernandes';
+
+
 
 --f)
 select intervencao, count(id) from (PESSOA join INTER_EQUIPA on Pessoa.equipa = INTER_EQUIPA.equipa) group by intervencao order by intervencao asc;
@@ -55,6 +68,15 @@ select DISTINCT PESSOA.nome, profissao, telefone from (PESSOA left outer join TE
 --3.f)
 
 --4
+--OBTER ID DE ACTIVO
+--select distinct id from ACTIVO where nome = 'válvula de ar condicionado'; 
+
+--INSERT INTERVENCAO PARA SUBSTITIR VALVULA 
+INSERT INTO INTERVENCAO(num, descricao, estado, dtinicio, dtfim, valcusto, activo, atrdisc)
+VALUES (10,'rutura','em execução','2021-12-20',NULL,150,5,'NP');
+
+--ATRIBUIR EQUIPA A SUBSTITUIÇÃO DE VALVULA
+INSERT INTO INTER_EQUIPA(intervencao, equipa) VALUES (10,1);
 
 --5
 
