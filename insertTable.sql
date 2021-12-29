113497fc718b3cc050a85bfc7ae64ee58d963fda
tp323 <tiagopardl@hotmail.com>
2021-12-29 19:25:56 +0000

    insert Table fix
    Fix to insert Table to follow up on changes on createTable

--- insertTable.sql
+++ insertTable.sql
@@ -30,11 +30,11 @@ INSERT INTO  TEL_EMPRESA(empresa, telefone)
 values(1,'999991999'),(2,'910000001');
 
 INSERT INTO ACTIVO(id, nome, estado, dtaquisicao, marca, modelo, localizacao, idactivotopo, tipo, empresa, pessoa)
-VALUES (1,'cena1','1','2021-02-02',NULL,NULL,'ali',1,1,1,2),
-(2,'cena2','1','2021-09-01',NULL,NULL,'aqui',1,2,1,2),
-(3,'cena3','0','2020-01-21',NULL,NULL,'acolá',2,3,2,5),
-(4,'cena4','1','2018-04-08',NULL,NULL,'ali ao longe',1,4,2,5),
-(5,'válvula de ar condicionado','0','2010-06-15','LG','xpto12.3','gabinete da direcção do Complexo de Piscinas',1,5,1,5);
+VALUES ('a0001','cena1','1','2021-02-02',NULL,NULL,'ali','a0001',1,1,2),
+('z0002','cena2','1','2021-09-01',NULL,NULL,'aqui','a0001',2,1,2),
+('b0003','cena3','0','2020-01-21',NULL,NULL,'acolá','a0001',3,2,5),
+('A0004','cena4','1','2018-04-08',NULL,NULL,'ali ao longe','a0001',4,2,5),
+('Z0005','válvula de ar condicionado','0','2010-06-15','LG','xpto12.3','gabinete da direcção do Complexo de Piscinas','a0001',5,1,5);
 
 INSERT INTO COMP_PESSOA(pessoa, competencia)
 VALUES (1,1),(1,2),(2,1),(3,2);
@@ -43,25 +43,27 @@ INSERT INTO TEL_PESSOA(pessoa, telefone)
 VALUES (1, '911111111');
 
 INSERT INTO INTERVENCAO(num, descricao, estado, dtinicio, dtfim, valcusto, activo, atrdisc)
-VALUES (1,'rutura','em análise','2021-03-03','2021-03-04',30,1,'P'),
-(2,'rutura','em análise','2021-07-09','2021-09-12',35,2,'P'),
-(3,'inspecção','em execução','2021-10-13',null,27,5,'NP'),
-(4,'rutura','em análise','2020-11-24',null,27,5,'NP'),
-(5,'rutura','em execução','2020-09-17',null,27,5,'NP');
+VALUES (1,'rutura','em análise','2021-03-03','2021-03-04',30,'a0001','P'),
+(2,'rutura','em análise','2021-07-09','2021-09-12',35,'z0002','P'),
+(3,'inspecção','em execução','2021-10-13',null,27,'Z0005','NP'),
+(4,'rutura','em análise','2020-11-24',null,27,'Z0005','NP'),
+(5,'rutura','em execução','2020-09-17',null,27,'Z0005','NP'),
+(6,'olear','em execução','2021-01-17',null,7,'a0001','NP'),
+(7,'olear','em execução','2022-01-02',null,7,'a0001','NP');
 
 INSERT INTO VCOMERCIAL(dtvcomercial, activo, valor)
-VALUES ('2021-02-03',1,23),
-('2021-04-04',1,25),
-('2021-12-20',1,22),
-('2021-10-21',1,13),
-('2021-09-01',2,52),
-('2020-01-22',3,120),
-('2018-04-09',4,35),
-('2010-06-15',5,15),
-('2021-01-26',3,125);
+VALUES ('2021-02-03','a0001',23),
+('2021-04-04','a0001',25),
+('2021-12-20','a0001',22),
+('2021-10-21','a0001',13),
+('2021-09-01','z0002',52),
+('2020-01-22','b0003',120),
+('2018-04-09','A0004',35),
+('2010-06-15','Z0005',15),
+('2021-01-26','b0003',125);
 
 INSERT INTO INTER_EQUIPA(intervencao, equipa)
-VALUES (1,1),(2,3),(3,2),(4,2),(5,1);
+VALUES (1,1),(2,3),(3,2),(4,2),(5,1),(6,3),(7,1);
 
 INSERT INTO INTER_PERIODICA(intervencao, periodicidade)
 VALUES (1,2),(2,6);
